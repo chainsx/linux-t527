00d8c3c8dc104d0219e5ec81947291ebbf2c666a
Hsun Lai <i@chainsx.cn>
2026-05-24 20:54:34 +0800

    fix sundi-drm

--- bsp/drivers/drm/sunxi_drm_drv.c
+++ bsp/drivers/drm/sunxi_drm_drv.c
@@ -1173,7 +1173,7 @@ static int sunxi_drm_procfs_status_show(struct seq_file *m, void *data)
 }
 static int sunxi_drm_procfs_create(void)
 {
-	proc_entry = proc_mkdir("sunxi-drm", NULL);
+	proc_entry = proc_mkdir(DRIVER_NAME, NULL);
 	if (IS_ERR_OR_NULL(proc_entry)) {
 		pr_err("Couldn't create sunxi-drm procfs directory !\n");
 		return -ENOMEM;
@@ -1194,7 +1194,7 @@ static int sunxi_drm_procfs_init(struct drm_device *drm)
 
 static void sunxi_drm_procfs_term(void)
 {
-	remove_proc_subtree("sunxi-drm", NULL);
+	remove_proc_subtree(DRIVER_NAME, NULL);
 	proc_entry = NULL;
 }
 
@@ -1362,7 +1362,7 @@ static struct platform_driver sunxi_drm_platform_driver = {
 	.remove = sunxi_drm_platform_remove,
 	.driver = {
 		.owner = THIS_MODULE,
-		.name = "sunxi-drm",
+		.name = DRIVER_NAME,
 		.of_match_table = sunxi_of_match,
 		.pm = &sunxi_drm_pm_ops,
 	},
